edd37c328a053b96fe5b2272a40aa22bcb57b483
BeiGod <saidsamuel2006@hotmail.com>
2023-04-09 23:45:29 -0500

    delete useless fragment

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -4,14 +4,12 @@ import GameCard from './GameCard'
 
 export default function GamesList () {
   return (
-    <>
-      <Grid>
-        {games.map(game => (
-          <Grid.Col xs={6} md={6} lg={4} key={game.id}>
-            <GameCard {...game} />
-          </Grid.Col>
-        ))}
-      </Grid>
-    </>
+    <Grid>
+      {games.map(game => (
+        <Grid.Col xs={6} md={6} lg={4} key={game.id}>
+          <GameCard {...game} />
+        </Grid.Col>
+      ))}
+    </Grid>
   )
 }
